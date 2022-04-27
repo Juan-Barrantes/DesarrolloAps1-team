51632fe624297a37342f1d739c009374e74bc235
Juan Barrantes <barrantesjuan513@gmail.com>
2022-04-26 19:15:53 -0500

    agregamos un query de insert en tb_tratantes

---
+++ insertTb_Tratante.sql
@@ -0,0 +1,22 @@
+use bd_NegocioAutosA1
+select * from tb_Tratante;
+
+	Insert Into tb_Tratante
+	values
+	 (1001, '1', 'Angelina', 'Woolland', '79289 Jackson Hill', '858673299'),
+	 (1002, '1', 'Kesley', 'Grainger', '55115 High Crossing Terrace', '758796167'),
+	 (1003, '1', 'Jamesy', 'Berkley', '49516 Transport Street', '962282040'),
+	 (1004, '2', 'Robinetta', 'Gadsdon', '1700 Golf View Crossing', '125947675'),
+	 (1005, '2', 'Allin', 'Basley', '1 Atwood Place', '316833940'),
+	 (1006, '2', 'Elbertina', 'Connal', '80 Hermina Place', '802657515'),
+	 (1007, '3', 'Emmaline', 'Bogies', '9 Bobwhite Place', '430573957'),
+	 (1008, '3', 'Laurens', 'Buddle', '73084 Lake View Place', '208242307'),
+	 (1009, '3', 'Marla', 'Navan', '8 Hagan Center', '3978026062'),
+	 (1010, '4', 'Bernadina', 'Brendeke', '84 Menomonie Hill', '395680212'),
+	 (1011, '4', 'Alfonso', 'Oliphant', '37149 Gina Street', '537358334'),
+	 (1012, '4', 'Holly-anne', 'Aylott', '94599 Jackson Court', '936140185'),
+	 (1013, '5', 'Tommy', 'Habbergham', '22 Pine View Court', '155548557'),
+	 (1014, '5', 'Essie', 'Kesey', '483 Cordelia Junction', '740590810'),
+	 (1015, '5', 'Rosabel', 'Golagley', '44181 Westend Lane', '859534551');
+
+	 select * from tb_Tratante;
